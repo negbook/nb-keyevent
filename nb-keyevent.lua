f05f6c3fc8b0dd7cfde8b80b823d984691af189e
negbook <negbook@gmail.com>
2022-07-07 21:59:13 +0800

    Update nb-keyevent.lua

--- nb-keyevent.lua
+++ nb-keyevent.lua
@@ -658,7 +658,7 @@ if GetCurrentResourceName() == "nb-keyevent" then
                 table.insert(hashes,`INPUT_RESPAWN_FASTER`)
                 table.insert(hashes,`INPUT_HUDMARKER_SELECT`)
                 local Keys = { ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18, ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182, ["LSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81, ["LCONTROL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RCONTROL"] = 70, ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178, ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173, ["NENTER"] = 201, ["NUMPAD4"] = 108, ["NUMPAD5"] = 60, ["NUMPAD6"] = 107, ["ADD"] = 96, ["SUBTRACT"] = 97, ["NUMPAD7"] = 117, ["NUMPAD8"] = 61, ["NUMPAD9"] = 118 }
-                print(hashes[Keys[string.upper(key)]+1],key)
+                
                 key = hashes[Keys[string.upper(key)]+1]
                 group = 0
             end 
