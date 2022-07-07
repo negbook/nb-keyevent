259e7badd022a9387dd6d2ed55abbb9558d5c9b1
negbook <negbook@gmail.com>
2022-07-07 21:50:33 +0800

    supported RedM
    Update nb-keyevent.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'adamant'
-game 'gta5'
+game {'gta5','rdr3'}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 version '1.0.0'
 author 'negbook'
